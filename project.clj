f8b07c5c38778cd8b842a0a7b71afe5566631ede
Despoina Sifouna <Despoina.Sifouna@kaluza.com>
2021-02-06 14:49:54 +0000

    fix standalone executable and detect language and translate text

--- project.clj
+++ project.clj
@@ -3,12 +3,15 @@
   :url "https://github.com/despinaki/translate-cli"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
-  :main ^:skip-aot translator-cli.core
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [com.google.cloud/google-cloud-translate "1.95.7"]]
+  :main translator-cli.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}}
-  :plugins [[lein-bin "0.3.5"]]
+                      ;  :jvm-opts ["-Dclojure.compiler.direct-linking=true"]
+                       }
+             :dev {:plugins [[lein-binplus "0.6.6"]]}}
   :bin {:name "translate" ;name of standalone executable
         :bin-path "~/Despoina.Sifouna/bin" ;where executable is placed
-        :bootclasspath true})
+        }
+  )
--- src/translator_cli/core.clj
+++ src/translator_cli/core.clj
@@ -1,7 +1,31 @@
 (ns translator-cli.core
+  (:import [com.google.cloud.translate TranslateOptions Translate$TranslateOption] ;ClassName$NestedClassName
+           [java.lang.reflect Array]) 
+  
   (:gen-class))
+;;             JAVA EXAMPLE
+;; import com.google.cloud.translate.*;
+;; Translate translate = TranslateOptions.getDefaultInstance().getService();       -->getDefaultInstance() is a class method (static), getService() is an object method
+;; Translation translation = translate.translate("¡Hola Mundo!");                  -->we dont need to use the Translate and Translation classes in Clojure (no need to declare the type)
+;; System.out.printf("Translated Text:\n\t%s\n", translation.getTranslatedText()); 
+
+(defn translate-text
+  "Use google cloud translate api to translate a text"
+  [lan text]
+  (let [translate (-> (TranslateOptions/getDefaultInstance) .getService) ;Get service using GOOGLE_APPLICATION_CREDENTIALS environment var
+        detection (.getLanguage (.detect translate text))
+        options (Array/newInstance Translate$TranslateOption 2)]
+    (do
+      (aset options 0 (Translate$TranslateOption/sourceLanguage detection))
+      (aset options 1 (Translate$TranslateOption/targetLanguage lan)))
+    ;; (println (vec (.getMethods (class translate))))
+    (println (.getTranslatedText (.translate translate text options)))
+    ;; (println detection)
+    ))
+
 
 (defn -main
-  "what i do"
-  [& args]
-  (println "Hello, World!"))
+  [lan text]
+  (if(and (string? lan) (string? text)) (translate-text lan text) (println "Try running: translate 'lan' 'text', where lan is the 2-letter code in string format and text is in string format also.")))
+;add another condition that checks if lang is in list
+;add exception handling
